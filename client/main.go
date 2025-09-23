0e6c228e42d2a4a468c83e876fcad2c97d18792f
komadiina <komadina.ognjen@gmail.com>
2025-09-23 15:48:45 +0200

    init protobufs

---
+++ client/main.go
@@ -0,0 +1,29 @@
+package client
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "github.com/komadiina/spelltext"
+	"google.golang.org/grpc"
+)
+
+func (s *pb.Server) SayHello(ctx context.Context, req *pb.Ping) (*pb.Pong, error) {
+	return &pb.Pong{Message: "Hello " + req.Name}, nil
+}
+
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server := grpc.NewServer()
+
+	err = server.Serve(lis)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
