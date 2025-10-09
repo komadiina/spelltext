3182d1359d635b297239bc8bb42024d19640100c
komadiina <komadina.ognjen@gmail.com>
2025-10-09 22:55:36 +0200

    config migration: docker -> k8s

--- server/chat/config/config.go
+++ server/chat/config/config.go
@@ -3,10 +3,10 @@ package config
 import "github.com/komadiina/spelltext/shared/config"
 
 type Config struct {
-	Port int `yaml:"port" env:"PORT" env-default:"50051"`
-	// NatsURL         string `yaml:"natsUrl" env:"NATS_URL" env-default:"nats://spelltext-nats:4222"`
-	NatsURL         string `yaml:"natsUrl" env:"NATS_URL" env-default:"nats://nats:4222"`
+	Port            int    `yaml:"port" env:"PORT" env-default:"50051"`
+	NatsURL         string `yaml:"natsUrl" env:"NATS_URL" env-default:"nats://spelltext-nats:4222"`
 	MaxAsyncPublish int    `yaml:"maxAsyncPublish" env:"MAX_ASYNC_PUBLISH" env-default:"512"`
+	// NatsURL         string `yaml:"natsUrl" env:"NATS_URL" env-default:"nats://nats:4222"`
 }
 
 func LoadConfig() (*Config, error) {
--- server/store/config/config.go
+++ server/store/config/config.go
@@ -3,15 +3,15 @@ package config
 import "github.com/komadiina/spelltext/shared/config"
 
 type Config struct {
-	ServicePort int `yaml:"port" env:"PORT" env-default:"50052"`
-	// PgUser      string `yaml:"pgUser" env:"PG_USER" env-default:"postgres"`
-	PgUser string `yaml:"pgUser" env:"PG_USER" env-default:"user"`
-	PgPass string `yaml:"pgPass" env:"PG_PASS" env-default:"changeme"`
-	// PgHost      string `yaml:"pgHost" env:"PG_HOST" env-default:"spelltext-postgresql-ha-pgpool.spelltext.svc.cluster.local"`
-	PgHost    string `yaml:"pgHost" env:"PG_HOST" env-default:"postgres"`
-	PgPort    int    `yaml:"pgPort" env:"PG_PORT" env-default:"5432"`
-	PgDbName  string `yaml:"pgDbName" env:"PG_DB_NAME" env-default:"spelltext"`
-	PgSSLMode string `yaml:"pgSslMode" env:"PG_SSL_MODE" env-default:"disable"`
+	// PgUser string `yaml:"pgUser" env:"PG_USER" env-default:"user"`
+	// PgHost    string `yaml:"pgHost" env:"PG_HOST" env-default:"postgres"`
+	ServicePort int    `yaml:"port" env:"PORT" env-default:"50052"`
+	PgUser      string `yaml:"pgUser" env:"PG_USER" env-default:"postgres"`
+	PgPass      string `yaml:"pgPass" env:"PG_PASS" env-default:"changeme"`
+	PgHost      string `yaml:"pgHost" env:"PG_HOST" env-default:"spelltext-postgresql-ha-pgpool.spelltext.svc.cluster.local"`
+	PgPort      int    `yaml:"pgPort" env:"PG_PORT" env-default:"5432"`
+	PgDbName    string `yaml:"pgDbName" env:"PG_DB_NAME" env-default:"spelltext"`
+	PgSSLMode   string `yaml:"pgSslMode" env:"PG_SSL_MODE" env-default:"disable"`
 }
 
 func LoadConfig() (*Config, error) {
