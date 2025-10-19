30d7bee73ac4bd2758cffbfd0de0952b5ef10143
komadiina <komadina.ognjen@gmail.com>
2025-10-19 23:12:52 +0200

    feat: authserver k8s chart

--- scripts/image/docker_ghcr.sh
+++ scripts/image/docker_ghcr.sh
@@ -6,6 +6,7 @@ docker build -t "ghcr.io/komadiina/spelltext/charserver:latest" -f server/charac
 docker build -t "ghcr.io/komadiina/spelltext/inventoryserver:latest" -f server/inventory/Dockerfile .
 docker build -t "ghcr.io/komadiina/spelltext/chatserver:latest" -f server/chat/Dockerfile .
 docker build -t "ghcr.io/komadiina/spelltext/gambaserver:latest" -f server/gamba/Dockerfile .
+docker build -t "ghcr.io/komadiina/spelltext/authserver:latest" -f server/auth/Dockerfile .
 
 docker push "ghcr.io/komadiina/spelltext/flyway:latest"
 docker push "ghcr.io/komadiina/spelltext/storeserver:latest"
@@ -13,3 +14,4 @@ docker push "ghcr.io/komadiina/spelltext/charserver:latest"
 docker push "ghcr.io/komadiina/spelltext/inventoryserver:latest"
 docker push "ghcr.io/komadiina/spelltext/chatserver:latest"
 docker push "ghcr.io/komadiina/spelltext/gambaserver:latest"
+docker push "ghcr.io/komadiina/spelltext/authserver:latest"
